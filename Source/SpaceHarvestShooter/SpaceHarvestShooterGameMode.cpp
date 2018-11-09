885ae764d71dba99103043332431ce09b0a64aee
MattLoveday99 <mattloveday99@gmail.com>
2018-11-09 15:48:31 +0000

    Initial Commit

---
+++ Source/SpaceHarvestShooter/SpaceHarvestShooterGameMode.cpp
@@ -0,0 +1,11 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "SpaceHarvestShooterGameMode.h"
+#include "SpaceHarvestShooterPawn.h"
+
+ASpaceHarvestShooterGameMode::ASpaceHarvestShooterGameMode()
+{
+	// set default pawn class to our character class
+	DefaultPawnClass = ASpaceHarvestShooterPawn::StaticClass();
+}
+

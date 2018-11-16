f39c8c849006419acfabd5a7fcda83fdbc54fada
Harry <harryl6661@gmail.com>
2018-11-16 15:41:57 +0000

    Reset branch

--- Source/SpaceHarvestShooter/SpaceHarvestShooterPawn.h
+++ Source/SpaceHarvestShooter/SpaceHarvestShooterPawn.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+
 #include "SpaceHarvestShooterPawn.generated.h"
 
 UCLASS(Blueprintable)
@@ -11,75 +12,23 @@ class ASpaceHarvestShooterPawn : public APawn
 {
 	GENERATED_BODY()
 
-	/* The mesh component */
-	UPROPERTY(Category = Mesh, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	class UStaticMeshComponent* ShipMeshComponent;
-
-	/** The camera */
-	UPROPERTY(Category = Camera, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* CameraComponent;
-
-	/** Camera boom positioning the camera above the character */
-	UPROPERTY(Category = Camera, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Weapons")
-		TSubclassOf<class AWeaponBase> WeaponType;
-
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Weapons")
-		AWeaponBase* Weapon;
-
-
 public:
 	ASpaceHarvestShooterPawn();
 
-	///** Offset from the ships location to spawn projectiles */
-	//UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite )
-	//FVector GunOffset;
-	//
-	///* How fast the weapon will fire */
-	//UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
-	//float FireRate;
-
-	/* The speed our ship moves around the level */
-	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
-	float MoveSpeed;
-
-	///** Sound to play each time we fire */
-	//UPROPERTY(Category = Audio, EditAnywhere, BlueprintReadWrite)
-	//class USoundBase* FireSound;
+	/** Sound to play each time we fire */
+	UPROPERTY(Category = Audio, EditAnywhere, BlueprintReadWrite)
+	class USoundBase* FireSound;
 
 	// Begin Actor Interface
-	virtual void Tick(float DeltaSeconds) override;
-	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
-	// End Actor Interface
+	void BeginPlay() override;
+	void Tick(float DeltaSeconds) override;
+	void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
-	///* Fire a shot in the specified direction */
-	//void FireShot(FVector FireDirection);
 
-	///* Handler for the fire timer expiry */
-	//void ShotTimerExpired();
-
-	// Static names for axis bindings
-	static const FName MoveForwardBinding;
-	static const FName MoveRightBinding;
-	static const FName FireForwardBinding;
-	static const FName FireRightBinding;
 
 private:
 
-	///* Flag to control firing  */
-	//uint32 bCanFire : 1;
+	void HandleMovement();
 
-	///** Handle for efficient management of ShotTimerExpired timer */
-	//FTimerHandle TimerHandle_ShotTimerExpired;
-
-public:
-	/** Returns ShipMeshComponent subobject **/
-	FORCEINLINE class UStaticMeshComponent* GetShipMeshComponent() const { return ShipMeshComponent; }
-	/** Returns CameraComponent subobject **/
-	FORCEINLINE class UCameraComponent* GetCameraComponent() const { return CameraComponent; }
-	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 };
 
